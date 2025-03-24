134e904546ff1e40086a66f8b4a8d59bcb4b9574
David Robert <contact@david-robert.fr>
2025-03-24 15:00:54 +0100

    publishedProducts variable

--- view/list-products.view.php
+++ view/list-products.view.php
@@ -4,16 +4,14 @@
 
 <main>
 
-<?php foreach($products as $product) { ?>
+<?php foreach($publishedProducts as $product) { ?>
 
-	<?php if ($product["isPublished"]) {?>
-		<p><?php echo $product["title"]; ?></p>
-		<p><?php echo $product["price"]; ?></p>
-		<p><?php echo $product["promotionPrice"]; ?></p>
-		<img src="<?php echo $product["image"]; ?>" />
-		<p><?php echo $product["category"]; ?></p>
-		<p><?php echo $product["publishedAt"]->format('Y-m-d'); ?></p>
-	<?php } ?>
+	<p><?php echo $product["title"]; ?></p>
+	<p><?php echo $product["price"]; ?></p>
+	<p><?php echo $product["promotionPrice"]; ?></p>
+	<img src="<?php echo $product["image"]; ?>" />
+	<p><?php echo $product["category"]; ?></p>
+	<p><?php echo $product["publishedAt"]->format('Y-m-d'); ?></p>
 
 <?php } ?>
 
