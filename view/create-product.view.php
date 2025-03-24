0f0e190be8012fe3183261dc4835fb3452adc07a
David Robert <contact@david-robert.fr>
2025-03-24 10:04:03 +0100

    create product controller + view

---
+++ view/create-product.view.php
@@ -0,0 +1,33 @@
+<html>
+
+
+<h1>Créer un produit</h1>
+
+
+<form>
+
+	<label>Titre
+		<input type="text" name="title">
+	</label>
+
+	<label>Prix
+		<input type="number" name="price">
+	</label>
+
+	<label>Prix promo
+		<input type="number" name="promoPrice">
+	</label>
+
+	<label>Catégorie
+		<input type="text" name="category">
+	</label>
+
+	<label>Image
+		<input type="text" name="image">
+	</label>
+
+	<button type="submit">Valider</button>
+
+</form>
+
+</html>
